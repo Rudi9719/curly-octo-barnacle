a03f877a206077f84ec2c53e5a348c2d02d1596a
Rudi <rudi@rudi-wow>
2016-02-07 00:44:22 -0500

    Build script added

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # TrinityCore build script - $1 = Cores to be used
-cmake TrinityCore/ -DCMAKE_INSTALL_PREFIX=/home/trinity/server -DWITH_WARNINGS=1
+cmake TrinityCore/ -DCMAKE_INSTALL_PREFIX=/wow/test/server -DWITH_WARNINGS=1
 make -j $1
 make install
