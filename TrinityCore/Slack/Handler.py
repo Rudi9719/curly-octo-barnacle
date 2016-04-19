286460454825ad35f1e1d05d4b6c19668b69481e
Gregory Rudolph <rudi@lolay.com>
2016-04-19 13:53:55 -0400

    Added Python Slack library

--- TrinityCore/Slack/Handler.py
+++ TrinityCore/Slack/Handler.py
@@ -1,38 +1,44 @@
 import os
 import time
 import re
-from slackclient import SlackClient
+from slackclient.slackclient import SlackClient
 
 token = "" #Slack token
-sc = SlackClient(token)
-test_message = "Command: .a Hello [Player: Rudi (GUID Full: 0x0000000000000001 Type: Player Low: 1) (Account: 1) X: 1887.870483 Y: -4423.166504 Z: 12.811410 Map: 1 (Kalimdor) Area: 1637 (Orgrimmar) Zone: Unknown Selected:  (GUID Full: 0x0000000000000000 Type: None Low: 0)]"
+#sc = SlackClient(token)
+test_message = "Command: .a Hello There!! This is my message :] [Player: Rudi (GUID Full: 0x0000000000000001 Type: Player Low: 1) (Account: 1) X: 1887.870483 Y: -4423.166504 Z: 12.811410 Map: 1 (Kalimdor) Area: 1637 (Orgrimmar) Zone: Unknown Selected:  (GUID Full: 0x0000000000000000 Type: None Low: 0)]"
 
 
 def message_strip(message):
-    message = re.sub(,'',message)
-
-def slack_listen():
-    if sc.rtm_connect()
-        while True:
-            #handle sc.rtm_read()
-            time.sleep(1)
-    else:
-        print "Connection to Slack Failed. Invalid token?"
-
+    command = message
+    player = message
+    command = re.search('Command: .a (.*) \[Player',  command)
+    # regex: 'Player: ([a-zA-Z]+)' $1
+    player = re.search('Player: ([a-zA-Z]+)' , player)
+    print(player.group(1) + ": " + command.group(1))
+    return player.group(1) + ": " + command.group(1)
 
 def start_auth():
+    pass
 
 def start_word():
+    pass
 
-def slack_to_world(message, user)
+def slack_to_world(message, user):
+    pass
 
-def world_to_slack(output)
-    message = ""
-    sc.rtm_send_message(["#wowserver", message])
+def world_to_slack(output):
+    message = message_strip(output)
+# sc.rtm_send_message(["#wowserver", message])
 
 
 def listen_to_world():
+    pass
 
 
 
 def main():
+    world_to_slack(test_message)
+
+
+if __name__ == '__main__':
+    main()
