54fcbb9e6329305b9d357aef332a15dd55b96343
Gregory Rudolph <rudi@lolay.com>
2016-04-19 22:12:46 -0400

    STDOUT?

--- Slack/Handler.py
+++ Slack/Handler.py
@@ -5,7 +5,7 @@ from subprocess import Popen, PIPE
 from slackclient import SlackClient
 
 
-world = Popen(["/wow/test/bin/worldserver"], stdin=PIPE, shell=True)
+world = Popen(["/wow/test/bin/worldserver"], stdin=PIPE, stdout=PIPE, shell=True)
 token = "" #Slack token
 #sc = SlackClient(token)
 test_message = "Command: .a Hello There!! This is my message :] [Player: Rudi (GUID Full: 0x0000000000000001 Type: Player Low: 1) (Account: 1) X: 1887.870483 Y: -4423.166504 Z: 12.811410 Map: 1 (Kalimdor) Area: 1637 (Orgrimmar) Zone: Unknown Selected:  (GUID Full: 0x0000000000000000 Type: None Low: 0)]"
