320526cef6f131ec07826e77dfc6961ff7c7ce37
Gregory Rudolph <rudi@lolay.com>
2016-04-20 21:58:51 -0400

    regex update

--- Slack/Test.py
+++ Slack/Test.py
@@ -49,7 +49,7 @@ def slack_to_world(message):
         else:
             user = re.search('\"user\": \"(\w)+\"', post)
             user = get_username(user)
-            message = re.search("\"text\": \"(.*)\", \"",  post)
+            message = re.search('\"text\": \"(.*)\", \"ts',  post)
             post = user + ": " + message.group(1)
             world.sendline("a " + post + "\n\r")
             world.sendline()
@@ -106,6 +106,7 @@ def get_username(user):
         return username.group(1)
     else:
         return "Slack"
+
 
 
 
